eaed54c6abc284b8710d4c94266cf82847364536
Christian Douglas Naranjo Yuncosa <chnaranj@c3r2s1.42madrid.com>
2024-09-30 16:44:33 +0200

    dd

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,40 +6,31 @@
 /*   By: chnaranj <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 17:22:57 by chnaranj          #+#    #+#             */
-/*   Updated: 2024/09/26 18:29:26 by chnaranj         ###   ########.fr       */
+/*   Updated: 2024/09/30 16:44:19 by chnaranj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdlib.h>
 #include <stddef.h>
 
-
 void	*ft_memmove(void *dest, const void *src, size_t len)
 {
-	char *d;
-	const char *s;
-	size_t i;
+	char		*d;
+	const char	*s;
+	size_t		i;
 
 	if (dest == NULL && src == NULL)
-	{
 		return (NULL);
-	}
-	
-
 	d = (char *) dest;
 	s = (const char *) src;
 	i = 0;
-
-	
 	if (d > s)
 	{
-		while(len-- > 0)
-		{
+		while (len-- > 0)
 			d[len] = s[len];
-		}
 	}
 	else
 	{
-		while(i < len)
+		while (i < len)
 		{
 			d[i] = s[i];
 			i++;
@@ -47,8 +38,3 @@ void	*ft_memmove(void *dest, const void *src, size_t len)
 	}
 	return (dest);
 }
-
-
-
-	
-
