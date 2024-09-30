783e2ccc9293ff4199390ac160ae03a71527223e
Christian Douglas Naranjo Yuncosa <chnaranj@c3r2s1.42madrid.com>
2024-09-30 16:25:56 +0200

    16 dde la tarde

---
+++ ft_bzero.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chnaranj <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/30 15:34:03 by chnaranj          #+#    #+#             */
+/*   Updated: 2024/09/30 16:25:38 by chnaranj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <strings.h>
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_bzero(void *s, size_t n)
+{
+	size_t			i;
+	unsigned char	*str;
+
+	i = 0;
+	str = (unsigned char *) s;
+	if (n == 0)
+		return ;
+	while (i < n)
+	{
+		str[i] = '\0';
+		i++;
+	}
+}
+/*
+int	main()
+{
+	char	s[20];
+	size_t	n;
+	n = 18;
+	ft_bzero(s, n);
+	return (0);
+}*/
+/*
+int main()
+{
+	char	s[] = "HOLA COMO TE VA LA MAÑANA DE HOY";
+	size_t	n;
+	n = 18;
+	ft_bzero(s, n);
+	printf(s, n);
+
+	int	cont;
+	cont = 0;
+
+	while(cont < 20)
+	{
+		printf("%d",s[cont]);
+		cont++;
+	}
+	return (0);
+}*/
