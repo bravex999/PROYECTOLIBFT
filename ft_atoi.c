164e8a320a972b0e441eef9ceb03d643a67c32d4
Christian Douglas Naranjo Yuncosa <chnaranj@c2r14s6.42madrid.com>
2024-10-05 13:43:41 +0200

    hhx

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,30 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chnaranj <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/05 13:36:49 by chnaranj          #+#    #+#             */
+/*   Updated: 2024/10/05 13:43:25 by chnaranj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include "libft.h"
 
-int ft_atoi(const char *nptr)
+int	ft_atoi(const char *nptr)
 {
-    int i;
-    int sign;
-    int result;
+	int	i;
+	int	sign;
+	int	result;
 
-    i = 0;
-    sign = 1;
-    result = 0;
-    while (nptr[i] == ' ' || (nptr[i] >= 9 && nptr[i] <= 13))
-        i++;
-    if (nptr[i] == '+' || nptr[i] == '-')
-    {
-        if (nptr[i] == '-')
-                sign = -1;
-        i++;
-    }
-    while (nptr[i] >= '0' && nptr[i] <= '9')
-    {
-        result = (result * 10) + (nptr[i] - '0');
-        i++;
-    }
-    result = sign * result;
-    return (result);
-}/*
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (nptr[i] == ' ' || (nptr[i] >= 9 && nptr[i] <= 13))
+		i++;
+	if (nptr[i] == '+' || nptr[i] == '-')
+	{
+		if (nptr[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		result = (result * 10) + (nptr[i] - '0');
+		i++;
+	}
+	result = sign * result;
+	return (result);
+}
+/*
 int main(int argc, char *argv[])
 {
     (void)argc;
