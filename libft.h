fd3eb8c37027361ac47ab2631b13ab8ee52d85e1
Christian Douglas Naranjo Yuncosa <chnaranj@c2r14s6.42madrid.com>
2024-10-05 16:30:18 +0200

    ffe

--- libft.h
+++ libft.h
@@ -1,13 +1,24 @@
-#ifndef	LIBFT_H
-#define LIBFT_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chnaranj <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/05 16:29:19 by chnaranj          #+#    #+#             */
+/*   Updated: 2024/10/05 16:30:00 by chnaranj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef LIBFT_H
+# define LIBFT_H
 
-#include <string.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
+# include <string.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
 void	*ft_memmove(void *dest, const void *src, size_t len);
 size_t	ft_strlen(const char *s);
-int ft_toupper(int c);
-
+int		ft_toupper(int c);
 
 #endif
