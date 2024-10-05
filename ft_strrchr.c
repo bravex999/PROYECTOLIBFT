4538fdcd670dbbe3bc5d079e817017f5f3239771
Christian Douglas Naranjo Yuncosa <chnaranj@c2r14s6.42madrid.com>
2024-10-05 14:29:53 +0200

    f

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,29 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chnaranj <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/05 14:15:57 by chnaranj          #+#    #+#             */
+/*   Updated: 2024/10/05 14:29:39 by chnaranj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include "libft.h"
 
-char    *ft_strrchr(char *s, int c)
-
+char	*ft_strrchr(char *s, int c)
 {
-    int i;
-    
-    i = 0;
-    
-    if (s)
-    {
-        while (s[i])
-            i++;
-    while(i >= 0 && s[i] != c)
-        i--;
-    if (i >= 0)
-        return (&s[i]);
-      }
-    return (NULL);
- }
- 
-  int main()
-  {
+	int	i;
+
+	i = 0;
+	if (s)
+	{
+		while (s[i])
+			i++;
+		while (i >= 0 && s[i] != c)
+			i--;
+		if (i >= 0)
+			return (&s[i]);
+	}
+	return (NULL);
+}
+/* 
+int main()
+{
       char    s[] = "Hola como estas";
       int c;
       c = 'o';
       printf("%s", ft_strrchr(s, c));
       return (0);
- }
+}*/
