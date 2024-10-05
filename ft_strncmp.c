1d2d87676b3c976ce65f3717960c5e97f9ebebf7
Christian Douglas Naranjo Yuncosa <chnaranj@c2r14s6.42madrid.com>
2024-10-05 13:49:05 +0200

    ggy

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,18 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chnaranj <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/05 13:45:59 by chnaranj          #+#    #+#             */
+/*   Updated: 2024/10/05 13:48:55 by chnaranj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include "libft.h"
 
-int ft_strncmp(const char *s1, const char *s2, size_t n)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-    size_t i;
-    
-    i = 0;
+	size_t	i;
 
-    if (n == 0)
-        return (0);
-    while (i < n && (s1[i] || s2[i]) && s1[i] == s2[i])
-        i++;
-    if (i == n && s1[i] == s2[i])
-        return (0);
-    return (s1[i] - s2[i]);
+	i = 0;
+	if (n == 0)
+		return (0);
+	while (i < n && (s1[i] || s2[i]) && s1[i] == s2[i])
+		i++;
+	if (i == n && s1[i] == s2[i])
+		return (0);
+	return (s1[i] - s2[i]);
 }
 /*
 int main ()
