bb8da19d407bbdf5e453bbc60ac8241a97a1029c
Bravex <cdny1987@gmail.com>
2024-10-06 19:45:18 +0200

    sdf

---
+++ ft_strnstr.c
@@ -0,0 +1,39 @@
+#include "libft.h"
+
+char    *ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+    if (!haystack || !needle || len == 0)
+	{
+		return (0);
+	}
+	size_t i;
+	size_t j;
+
+	i = 0;
+	if (!needle[0])
+		return ((char *)haystack);
+	while (haystack[i] != '\0' && i < len)
+	{
+		j = 0;
+		while (haystack[i + j] == needle[j] && (i + j) < len)
+		{
+			if (!needle[j + 1])
+				return ((char *)&haystack[i]);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+/*
+int main()
+{
+	const char haystack[] = "123abc123";
+	const char needle[] = "abc";
+	size_t len;
+	len = 1;
+	char *result;
+	result = ft_strnstr(haystack, needle, len);
+	printf ("%s", result);
+	return (0);
+}*/
