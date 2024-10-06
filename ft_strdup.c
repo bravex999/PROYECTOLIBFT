8789623f87c52ea182d2c57f54aded4c081ef5c8
Bravex <cdny1987@gmail.com>
2024-10-06 21:23:25 +0200

    ff

---
+++ ft_strdup.c
@@ -0,0 +1,27 @@
+#include "libft.h"
+
+char *ft_strdup(const char *s)
+{
+	int i;
+	char *dup;
+	size_t len;
+
+	i = 0;
+	len = ft_strlen(s);
+	dup = ((char *) malloc(len + 1));
+	if (!dup)
+		return (NULL);
+	while (s[i] != '\0')
+	{
+		dup[i] = s[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
+int  main ()
+{
+	const char	*s = "hola como estas";
+	printf("%s", ft_strdup(s));
+	return (0);
+}
