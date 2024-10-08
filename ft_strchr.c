5815a3ef1f2c3928350b08e7ee03619a4170ea09
Christian Douglas Naranjo Yuncosa <chnaranj@c2r19s4.42madrid.com>
2024-10-08 14:45:49 +0200

    ffd

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: chnaranj <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 14:30:33 by chnaranj          #+#    #+#             */
-/*   Updated: 2024/10/05 14:33:57 by chnaranj         ###   ########.fr       */
+/*   Updated: 2024/10/08 14:45:23 by chnaranj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -21,7 +21,7 @@ char	*ft_strchr(const char *s, int c)
 		while (s[i] && s[i] != c)
 			i++;
 		if (s[i] == c)
-			return ((char*)&s[i]);
+			return ((char *)&s[i]);
 	}
 	return (NULL);
 }
