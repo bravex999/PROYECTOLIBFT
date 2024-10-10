9da0694930fb40617954fee1b15c40501b2e1998
Bravex <cdny1987@gmail.com>
2024-10-10 14:12:31 +0200

    dflskdfn

--- ft_striteri.c
+++ ft_striteri.c
@@ -1,33 +1,34 @@
 #include "libft.h"
 
-void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+void ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
-	unsigned int	i;
+    unsigned int i;
 
-	if (!s)
-		return;
-	if (!f)
-		return;
-	i = 0;
-	while (s[i])
-	{
-		f(i, &s[i]);
-		i++;
-	}
+    if (!s)
+        return;
+    if (!f)
+        return;
+    i = 0;
+    while (s[i])
+    {
+        f(i, &s[i]);
+        i++;
+    }
 }
 
-void	to_uppercase(unsigned int i, char *c)
+int main(void)
 {
-	(void)i;
-	if (*c >= 'a' && *c <= 'z')
-		*c -= 32;
+    char str[] = "hola, mundo!";
+
+    void to_uppercase(unsigned int i, char *c)
+    {
+        (void)i;
+        if (*c >= 'a' && *c <= 'z')
+            *c -= 32;
+    }
+
+    ft_striteri(str, to_uppercase);
+    printf("%s\n", str);
+    return 0;
 }
 
-int	main(void)
-{
-	char	str[] = "hola, mundo!";
-
-	ft_striteri(str, to_uppercase);
-	printf("%s\n", str);
-	return (0);
-}
